54d2cd12701d670207ea3465a2a57f06ac33412d
Jose Aviles <aviles.yanes7@gmail.com>
2022-09-28 19:56:48 -0600

    Initial folder structure, react router, redux and react-redux added

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,5 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+
+import HomePage from "./pages/homePage/homePage";
+import CharactersPage from "./pages/charactersPage/CharactersPage";
+import CharacterPage from "./pages/characterPage/CharacterPage";
+import ComicsPage from "./pages/comicsPage/ComicsPage";
+import StoriesPage from "./pages/storiesPage/StoriesPage";
+import ErrorPage from "./pages/errorPage/ErrorPage";
+
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="characters" element={<CharactersPage />} />
+        <Route path="character" element={<CharacterPage />} />
+        <Route path="comics" element={<ComicsPage />} />
+        <Route path="stories" element={<StoriesPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- marvel/src/index.js
+++ marvel/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
-
